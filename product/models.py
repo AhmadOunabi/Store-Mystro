9b61f53edb15751ea110bfe682b1d299c45ab1f4
AhmadOunabi <ahmad_ounabi@yahoo.com>
2023-06-14 18:57:30 +0200

    Add Models

--- product/models.py
+++ product/models.py
@@ -1,3 +1,21 @@
 from django.db import models
-
+from django.utils import timezone
+from taggit.managers import TaggableManager
 # Create your models here.
+
+class Product(models.Model):
+    name=models.CharField(max_length=50)
+    details=models.TextField(max_length=500)
+    prise=models.DecimalField(max_digits=5, decimal_places=2)
+    tags = TaggableManager()
+    image= models.ImageField(upload_to='photos/%y/%m/%d')
+    def __str__(self):
+        return self.name
+    
+class Reviews(models.Model):
+    product=models.ForeignKey(Product, related_name='product_review',on_delete=models.CASCADE)
+    review=models.TextField(max_length=500)
+    rate=models.DecimalField(max_digits=5, decimal_places=2)
+    timezone=models.DateTimeField(default=timezone.now)
+    def __str__(self):
+        return self.review
