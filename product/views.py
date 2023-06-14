a68868b5e410ec860e6b5f5a2c69d83dad0cc35d
AhmadOunabi <ahmad_ounabi@yahoo.com>
2023-06-14 19:28:12 +0200

    Add lists

--- product/views.py
+++ product/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from product.models import Product,Reviews
 # Create your views here.
+
+def list(request):
+    product_list = Product.objects.all()
+    context={'product_list':product_list}
+    return render(request,'list.html', context)
